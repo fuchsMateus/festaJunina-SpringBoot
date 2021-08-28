7b07d08d4f50e1e49905699d393bfb63efcad9dd
Mateus Fuchs <mateus5fuchs@gmail.com>
2021-08-28 02:05:18 -0300

    Funcionalidades de inserir e remover salgados adicionadas

--- src/main/java/com/mateusfuchs/festajuninasb/services/SalgadoService.java
+++ src/main/java/com/mateusfuchs/festajuninasb/services/SalgadoService.java
@@ -3,8 +3,10 @@ package com.mateusfuchs.festajuninasb.services;
 import com.mateusfuchs.festajuninasb.entities.Salgado;
 import com.mateusfuchs.festajuninasb.repositories.SalgadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,6 +22,10 @@ public class SalgadoService {
     public Salgado findById(Integer id){
         Optional<Salgado> salgadoOptional = repo.findById(id);
         return salgadoOptional.orElse(null);
+    }
+
+    public List<Salgado> findAll(){
+        return repo.findAll(Sort.by(Sort.Direction.ASC, "tipo"));
     }
 
     public Salgado save(Salgado salgado){
---
+++ src/main/resources/static/js/scripts.js
@@ -0,0 +1,49 @@
+function deleteSalgado(id){
+    $.ajax({
+        async: false,
+        url: "/deletar",
+        type: "DELETE",
+        data: JSON.stringify(id),
+        contentType: "application/json; charset=utf-8",
+    }).done(function (){
+        alert("Salgado removido com sucesso!");
+    }).fail(function (){alert("O salgado não existe ou não foi removido corretamente.")});
+
+    document.location.reload(true);
+}
+
+function saveSalgado(){
+
+    let tipo = $('#SelectTipo').val();
+    let sabor = $('#SelectSabor').val();
+
+    const rbs = document.querySelectorAll('input[name="fritoOuAssado"]');
+    let fritoOuAssado;
+    for (const rb of rbs) {
+        if (rb.checked) {
+            fritoOuAssado = rb.value;
+            break;
+        }
+    }
+
+    let preco = $('#InputPreco').val();
+    let quantidade = $('#InputQuant').val();
+
+    $.ajax({
+        async: false,
+        url: "/cadastro",
+        type: "POST",
+        data: JSON.stringify({
+            id : "",
+            tipo : tipo,
+            sabor : sabor,
+            fritoOuAssado : fritoOuAssado,
+            preco : preco,
+            quantidade : quantidade}),
+        contentType: "application/json; charset=utf-8",
+    }).done(function (){
+        alert("Salgado salvo com sucesso!");
+    }).fail(function (){alert("O salgado não existe ou não foi removido corretamente.")});
+
+    document.location.reload(true);
+}
